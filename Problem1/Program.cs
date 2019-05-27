350e33bfe5fc7def10d27da35ac6a95dcc89150b
ckmaster <ckidd0792@gmail.com>
2019-05-27 01:37:48 -0500

    hello world

---
+++ Problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fib1 = 1;
+            int fib2 = 2;
+            int sum = 0;
+            while (true)
+            {
+                if ((fib2 < 4000000) && (fib2 % 2 == 0))
+                {
+                    sum += fib2;
+                }
+                else if (fib2 > 4000000)
+                {
+                    break;
+                }
+                int temp = fib2;
+                fib2 = fib1 + fib2;
+                fib1 = temp;
+            }
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
