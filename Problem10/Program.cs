c55ca9c3f6d5bb1870ca76503dc39d5d5f0b85b9
ckmaster <ckidd0792@gmail.com>
2019-05-28 01:24:18 -0500

    dwa

---
+++ Problem10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int upperLimit = 2000000;
+            long sum = 5;
+            for (int i = 5; i < upperLimit; i+=2)
+            {
+                Console.WriteLine(i.ToString());
+                if (IsPrime(i))
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+        static bool IsPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Problem9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Problem9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 1000;
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            for (int i = 999; i > 0; i--)
+            {
+                for (int j = 2; j < i; j ++)
+                {
+                    for (int k = 1; k < j; k++)
+                    {
+                        if (((k*k + j*j == i*i)) && (i + j + k == sum))
+                        {
+                            c = i;
+                            b = j;
+                            a = k;
+                        }
+                    }
+                }
+            }
+            int product = a * b * c;
+            Console.WriteLine(product.ToString());
+        }
+    }
+}
